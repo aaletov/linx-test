f38294b5cf8604bffb40f54098d27b3081af1caa
aapozd <aapozdnyakove@gmail.com>
2022-10-19 01:53:40 +0300

    Add Product type and goodness calculation func
    Signed-off-by: aapozd <aapozdnyakove@gmail.com>

---
+++ pkg/product/product.go
@@ -0,0 +1,11 @@
+package product
+
+type Product struct {
+	Name   string `json:"product"`
+	Price  int
+	Rating int
+}
+
+func (p Product) LessGood(other Product) bool {
+	return (p.Price < other.Price) || ((p.Price == other.Price) && (p.Rating < other.Rating))
+}
