607f1d95aab096ded84e93519c6142a0ba64ceb7
aaletov <aapozdnyakove@gmail.com>
2022-10-19 05:04:41 +0300

    Add pretty print for Product
    Signed-off-by: aaletov <aapozdnyakove@gmail.com>

--- pkg/product/product.go
+++ pkg/product/product.go
@@ -1,9 +1,17 @@
 package product
 
+import (
+	"strconv"
+)
+
 type Product struct {
 	Name   string `json:"product" csv:"Product"`
 	Price  int
 	Rating int
+}
+
+func (p Product) String() string {
+	return p.Name + " " + strconv.Itoa(p.Price) + " " + strconv.Itoa(p.Rating)
 }
 
 func (p Product) LessGood(other Product) bool {
